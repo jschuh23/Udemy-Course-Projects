f7a50ddb752823b82f3a9a8c0c338f670d38bdcc
jschuh23 <janelleschuh@gmail.com>
2021-02-15 16:47:47 -0500

    Adjusted hamburger menu

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,16 @@
 const hamburger = document.querySelector('.nav__hamburger');
 const navList = document.querySelector('.nav__list');
 const links = document.querySelectorAll('.nav__list--item');
+let menuOpen = false;
 
 hamburger.addEventListener('click', () => {
+  if (!menuOpen) {
+    hamburger.classList.add('open');
+    menuOpen = true;
+  } else {
+    hamburger.classList.remove('open');
+    menuOpen = false;
+  }
+
   navList.classList.toggle('open');
 });
